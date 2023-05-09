9e62ad9a2832710c0e8dd69cc617ba30a4159e7e
Ryan Peelman <ryan.peelman@gmail.com>
2023-05-09 13:47:44 -0500

    add skeleton for additional endpoints

--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Response } from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
 
 @Controller('words')
 export class WordsController {
-  @Get()
-  async getJson(@Res() res: Response) {
+  async getWords(): Promise<string[]> {
     const filePath = path.resolve(__dirname, '..', 'assets', 'words.json');
     const file = fs.readFileSync(filePath);
-    return res.send(JSON.parse(file.toString()));
+    const json = JSON.parse(file.toString());
+
+    return json.json();
+  }
+
+  @Get()
+  async getAll(@Res() response: Response) {
+    const words = await this.getWords();
+    return response.send(words);
+  }
+
+  @Get('count')
+  async getByCount(@Req() request: Request, @Res() response: Response) {
+    const words = await this.getWords();
+    return response.send(words);
+  }
+
+  @Get('letter')
+  async getByLetter(@Req() request: Request, @Res() response: Response) {
+    const words = await this.getWords();
+    return response.send(words);
   }
 }
