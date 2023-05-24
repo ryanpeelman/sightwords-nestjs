5ed221b1f3038c30e636d90654377db21e639978
Ryan Peelman <ryan.peelman@gmail.com>
2023-05-24 09:16:56 -0500

    consolidate API to a single endpoint; introduce Swagger

--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { WordsService } from './words.service';
 
 @Controller('words')
@@ -6,20 +7,46 @@ export class WordsController {
   constructor(private readonly wordsService: WordsService) {}
 
   @Get()
-  async getAll(): Promise<string[]> {
-    const words = await this.wordsService.getWords();
+  @ApiOperation({
+    summary:
+      'Retrieve all sightwords; optionally constrained to those that start with a specified letter and/or a specified amount of words to return',
+  })
+  @ApiQuery({
+    name: 'count',
+    description: 'count',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'letter',
+    description: 'letter',
+    required: false,
+    type: String,
+  })
+  async getAll(
+    @Query('count') count?: number,
+    @Query('letter') letter?: string,
+  ): Promise<string[]> {
+    let words = await this.wordsService.getWords();
+
+    if (letter) {
+      words = await this.getByLetter(letter, words);
+    }
+
+    if (count) {
+      words = await this.getByCount(count, words);
+    }
+
     return words;
   }
 
-  @Get('count/:count')
-  async getByCount(@Param('count') count: number): Promise<string[]> {
-    const words = await this.wordsService.getWords();
+  async getByCount(count: number, source: string[]): Promise<string[]> {
+    const words = source ?? (await this.wordsService.getWords());
     return words.slice(0, count);
   }
 
-  @Get('letter/:letter')
-  async getByLetter(@Param('letter') letter: string): Promise<string[]> {
-    const words = await this.wordsService.getWords();
+  async getByLetter(letter: string, source: string[]): Promise<string[]> {
+    const words = source ?? (await this.wordsService.getWords());
     return words.filter((w) => w.startsWith(letter));
   }
 }
